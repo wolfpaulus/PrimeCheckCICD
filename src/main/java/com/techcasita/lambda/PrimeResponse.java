f70fbcdd8a4305f31dfed15b2316f8fdd6936b9a
Wolf Paulus <wolf@wolfpaulus.com>
2018-10-17 11:12:18 -0700

    Prep for next demo

--- src/main/java/com/techcasita/lambda/PrimeResponse.java
+++ src/main/java/com/techcasita/lambda/PrimeResponse.java
@@ -4,8 +4,7 @@
  */
 package com.techcasita.lambda;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
 
 import java.util.Date;
 
@@ -13,9 +12,8 @@ import java.util.Date;
 public class PrimeResponse {
     private static final Logger log = LogManager.getLogger(PrimeResponse.class);
     private final String tag = "PrimeCheck P4";
-    private final String theStamp = new Date().toString();
     private final String answer;
-    private final String copyright = "(c) 2018 GCCCD";
+
     private long n;
     private long d;
     public PrimeResponse(final long n, final long d) {
